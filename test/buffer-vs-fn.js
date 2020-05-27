a59d75979ba8789fde53c513012fe0e947d0741e
Jamie Perkins <jamie.perkins@gmail.com>
2020-05-27 11:24:04 -0600

    test function against buffer

---
+++ test/buffer-vs-fn.js
@@ -0,0 +1,21 @@
+// test string length function against Buffer
+var assert = require("assert"),
+  Buffer = require("buffer").Buffer;
+
+function lengthInUtf8Bytes(str) {
+  var m = encodeURIComponent(str).match(/%[89ABab]/g);
+  return str.length + (m ? m.length : 0);
+}
+function bufferSize(str) {
+  return Buffer.from(str).length;
+}
+describe("string byte length function is accurate", () => {
+  it("produces the same result as using Buffer", () => {
+
+    const tests = ["Hello world", "åß∂ƒ", "ثبهخنى", "你好", "ха ха...шутка что ли?"];
+
+    tests.forEach(item => {
+      assert.equal(lengthInUtf8Bytes(item), bufferSize(item));
+    });
+  });
+});
