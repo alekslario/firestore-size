5c6aa0b6d734c0af44e90d36afaa4fe3b629b31f
Jamie Perkins <jamie.perkins@gmail.com>
2020-05-28 05:55:12 -0600

    added Blob check and updated byteLength function

--- test/buffer-vs-fn.js
+++ test/buffer-vs-fn.js
@@ -2,9 +2,15 @@
 var assert = require("assert"),
   Buffer = require("buffer").Buffer;
 
-function lengthInUtf8Bytes(str) {
-  var m = encodeURIComponent(str).match(/%[89ABab]/g);
-  return str.length + (m ? m.length : 0);
+function byteLength(str) {
+  // returns the byte length of an utf8 string
+  var s = str.length;
+  for (var i=str.length-1; i>=0; i--) {
+    var code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s+=2;
+  }
+  return s;
 }
 function bufferSize(str) {
   return Buffer.from(str).length;
@@ -15,7 +21,7 @@ describe("string byte length function is accurate", () => {
     const tests = ["Hello world", "åß∂ƒ", "ثبهخنى", "你好", "ха ха...шутка что ли?", ""];
 
     tests.forEach(item => {
-      assert.equal(lengthInUtf8Bytes(item), bufferSize(item));
+      assert.equal(byteLength(item), bufferSize(item));
     });
   });
 });
